14cff169e4ee5501cb49882d22fb91ad8bb5f0ed
Kappocha <isaacbg2003@gmail.com>
2025-09-23 12:19:32 +0200

    MInitalk

--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,31 +6,20 @@ void ft_handler(int sig, siginfo_t *info, void *context)
     static int bit_count = 0;
     (void)context;
     
-    // Recibir bit: SIGUSR1 = 0, SIGUSR2 = 1
     if (sig == SIGUSR1)
         current_char = (current_char << 1) | 0;
     else if (sig == SIGUSR2)
         current_char = (current_char << 1) | 1;
-    
     bit_count++;
-    
-    // Si hemos recibido 8 bits (1 carácter completo)
     if (bit_count == 8)
     {
-        // Si es carácter nulo, fin del mensaje
         if (current_char == 0)
         {
             ft_putstr("\n");
-            // Enviar confirmación al cliente
             kill(info->si_pid, SIGUSR1);
         }
         else
-        {
-            // Imprimir el carácter recibido
             write(1, &current_char, 1);
-        }
-        
-        // Resetear para el siguiente carácter
         current_char = 0;
         bit_count = 0;
     }
@@ -43,19 +32,12 @@ int main(void)
     ft_putstr("ID del servidor: ");
     ft_putnbr(getpid());
     ft_putstr("\nEsperando mensajes...\n");
-    
-    // Configurar el manejador de señales
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = ft_handler;
     sigemptyset(&sa.sa_mask);
-    
-    // Registrar los manejadores
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
-    
-    // Bucle infinito esperando señales
     while (1)
         pause();
-        
     return (0); 
 }
